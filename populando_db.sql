c5e3d3444ee40fdbcebeac068adbb9d0cf27b43a
Diego Aguiar <diego.fernandes.aguiar@gmail.com>
2019-08-06 23:07:54 -0300

    Script SQL para a carga das tabelas do BD

---
+++ populando_db.sql
@@ -0,0 +1,16 @@
+INSERT INTO metatron_db.cargo values (1, "Programador");
+INSERT INTO metatron_db.cargo values (2, "Programador Front");
+INSERT INTO metatron_db.cargo values (3, "DBA");
+
+INSERT INTO metatron_db.usuario values (1, "Alberto", 1);
+INSERT INTO metatron_db.usuario values (2, "Carlos", 1);
+INSERT INTO metatron_db.usuario values (3, "Joao", 2);
+INSERT INTO metatron_db.usuario values (4, "Gabriel", 3);
+INSERT INTO metatron_db.usuario values (5, "Felipe", 3);
+
+INSERT INTO metatron_db.telefone values (1, "21", "12345678", 1);
+INSERT INTO metatron_db.telefone values (2, "21", "12345678", 2);
+INSERT INTO metatron_db.telefone values (3, "21", "12345678", 3);
+INSERT INTO metatron_db.telefone values (4, "21", "12345678", 4);
+INSERT INTO metatron_db.telefone values (5, "21", "12345678", 5);
+
